aac26e5a0c6c6559d60a0dee4bbb14be1fe3bbd2
Titus Soporan <titus@tsoporan.com>
2025-11-22 16:23:18 -0500

    Remove references for socialtide
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   // Update this to your client's domain
-  site: "https://CLIENT_NAME.socialtide.ai",
+  site: "https://your-domain.com",
 
   // Always use trailing slashes to avoid redirects on Cloudflare
   trailingSlash: "always",
